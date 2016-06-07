343409d5932ebe22d99967b3a7c238d294aded2f
Finble <elainefinn@hotmail.com>
2016-06-07 17:04:38 +0100

    User Story 1 - card has a 0 balance + User Story 2 - can add money to card, completed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,34 @@
-describe Oystercard do  
+require 'oystercard'
+
+describe Oystercard do 
+
+# User Story 1
+##############
+# In order to use public transport
+# As a customer
+# I want money on my card
+
+	it 'has initial balance' do 
+		expect(subject.balance).to eq(0)
+	end
+
+# User Story 2
+##############
+# In order to keep using public transport
+# As a customer
+# I want to add money to my card
+
+	describe '#top up' do  
+
+		it 'responds to #top up' do
+		# it {is_expected.to respond_to(:top_up).with(1).argument}
+		 	expect {subject.to respond_to(:top_up).with(1).argument}
+		end
+
+		it 'can increase the balance' do  
+			expect { subject.top_up(1)}.to change{ subject.balance }.by(1)
+		end
+
+	end
+
 end
