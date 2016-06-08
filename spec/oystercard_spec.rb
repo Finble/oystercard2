1b526dcd02c728e9a6bdf41d11ecfe258153030e
Finble <elainefinn@hotmail.com>
2016-06-08 14:23:46 +0100

    Refactored oystercard_spec.rb + removed SPARE.md

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,90 +2,19 @@ require 'oystercard'
 
 describe Oystercard do 
 
-# User Story 1
-##############
-# I want money on my card
-
 	it 'has initial balance' do 
 		expect(subject.balance).to eq(0) #starting balance of 0
 	end
 
-# User Story 6
-##############
-# I need to have the minimum amount (£1) for a single journey.
-
 	it 'cannot touch in if card is below the minimum balance' do  
 		expect{subject.touch_in}.to raise_error 'Insufficient balance to touch in'
 	end
-
-# User Story 3
-##############
-# I want a maximum limit (of £90) on my card
 
 	it 'raises an error if the maximum balance is exceeded' do  
 		maximum_balance = Oystercard::MAXIMUM_BALANCE
 		subject.top_up(maximum_balance)   #set balance to be at maximum so when 1 is added, in test below, it exceeds maximum and will raise error
 		expect{subject.top_up(1)}.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
 	end
-		
-		# User Story 2
-		##############
-		# I want to add money to my card
-
-			describe '#top up' do  
-
-				it {is_expected.to respond_to(:top_up).with(1).argument}
-
-				it 'can increase the balance' do  
-					expect {subject.top_up(1)}.to change{ subject.balance}.by(1)
-				end
-
-			end
-
-			# User Story 4
-			##############
-			# I need my fare deducted from my card - TEST DELETED AS METHOD MOVED TO PRIVATE (don't need to test for private methods as implicit with tests on methods that rely on it)
-
-			# describe '#deduct' do  
-
-			# 	it {is_expected.to respond_to(:deduct).with(1).argument}
-
-			# 	it 'can decrease the balance' do  
-			# 		subject.top_up(20)  # default balance = 0 so need to add an amount so the amount to be deducted can be tested
-			# 		expect{subject.deduct(3)}.to change{ subject.balance}.by(-3)
-			# 	end
-			# end
-
-	# User Story 5
-	##############
-	# I need to touch in and out.
-
-	it {is_expected.to respond_to(:touch_in)}
-
-	it 'can touch in' do 
-		subject.top_up(10) # new test (user story 6) defaults balance to < 1 so cannot touch in, so need to top up card to run this test
-		subject.touch_in
-		expect(subject).to be_in_journey
-	end
-
-	it {is_expected.to respond_to(:touch_out)}
-
-	it 'can touch out' do
-		subject.top_up(10) # new test (user story 6) defaults balance to < 1 so cannot touch in, so need to top up card to run this test
-		subject.touch_in
-		subject.touch_out
-		expect(subject).not_to be_in_journey
-	end
-
-	it {is_expected.to respond_to(:in_journey?)}
-
-	it 'is initially not in a journey' do  
-		expect(subject.in_journey?).to eq false
-	end
-
-	# User Story 7
-	##############
-	# I need the correct amount deducted from my card
 
 	it 'can charge card with amount for journey' do  
 		subject.top_up(20) # to counter the min balance of less than 1 default value
@@ -93,4 +22,41 @@ describe Oystercard do
 		expect{subject.touch_out}.to change {subject.balance}.by(-Oystercard::MINIMUM_BALANCE)
 	end
 
+		context '#top up' do  
+
+			it {is_expected.to respond_to(:top_up).with(1).argument}
+
+			it 'can increase the balance' do  
+				expect {subject.top_up(1)}.to change{ subject.balance}.by(1)
+			end
+
+		end
+
+		context '#touch in, #touch out, #in journey' do  
+
+			before :each do
+				subject.top_up(10)
+			end
+
+			it {is_expected.to respond_to(:touch_in)}
+
+			it 'can touch in' do 
+				subject.touch_in
+				expect(subject).to be_in_journey
+			end
+
+			it {is_expected.to respond_to(:touch_out)}
+
+			it 'can touch out' do
+				subject.touch_in
+				subject.touch_out
+				expect(subject).not_to be_in_journey
+			end
+
+			it {is_expected.to respond_to(:in_journey?)}
+
+			it 'is initially not in a journey' do  
+				expect(subject.in_journey?).to eq false
+			end
+		end
 end
